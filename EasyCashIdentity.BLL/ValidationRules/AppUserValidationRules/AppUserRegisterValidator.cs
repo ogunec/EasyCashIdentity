9d27b9f0c96495db5b0d884d2a6e6c4f8a3bfb43
Onur Güneç <ogunec@hotmail.com>
2023-06-29 23:35:12 +0300

    repository pattern created

---
+++ EasyCashIdentity.BLL/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,14 @@
+﻿using EasyCashIdentity.DTO.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentity.BLL.ValidationRules.AppUserValidationRules
+{
+    internal class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+    }
+}
